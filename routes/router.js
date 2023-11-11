541d0a806f5e58699220026fad6e646d9c074654
Jakov <sikiric.jakov@outlook.com>
2023-11-11 20:10:15 +0100

    help

--- routes/router.js
+++ routes/router.js
@@ -6,4 +6,25 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.post("/reset-passwords", async (req, res) => {
+  const sql = `UPDATE USERS
+  SET password = 
+      CASE 
+          WHEN username = 'Bob' THEN 'teardrop01'
+          WHEN username = 'Michael' THEN 'bulls1991'
+          WHEN username = 'Leo' THEN 'goat10'
+          WHEN username = 'Alice' THEN 'sunshine02'
+      END
+  WHERE username IN ('Bob', 'Michael', 'Leo', 'Alice');
+  `;
+  try {
+    const result = await db.query(sql, []);
+    console.log(result);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.redirect(500, "/");
+  }
+});
+
 export default router;
